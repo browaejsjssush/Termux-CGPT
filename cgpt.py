60050ef3f92ccdb5944f5f0bd75cffe727ff1d43
browaejsjssush <140291494+browaejsjssush@users.noreply.github.com>
2023-07-26 02:59:09 -0400

    Create cgpt.py
    the code

---
+++ cgpt.py
@@ -0,0 +1,28 @@
+print ("CHATGPT Bot")
+print ("Credits: Pydroid3 pip, Codingthe smartway.com")
+print ("WARNING: bot may spew nonsense")
+print ("WARNING: this version of the chatbot is very old, this means its very untrustworthy")
+print (".")
+print ("Enter your text:")
+	
+import openai
+
+openai.api_key = "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"
+
+
+
+model_engine = "text-davinci-003"
+prompt = input("")
+
+
+completion = openai.Completion.create(
+    engine=model_engine,
+    prompt=prompt,
+    max_tokens=1024,
+    n=1,
+    stop=None,
+    temperature=0.5,
+)
+
+response = completion.choices[0].text
+print(response)
